a571aa195fbf21c76e4e88c27806a7d303d3f0a8
amohamed176 <amohamed176@myseneca.ca>
2025-02-03 16:40:42 -0500

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#name = 'Jon'
+#age = 20
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+# Prompt the user for their name
+name = input("Name: ")
+
+# Prompt the user for their age
+age = input("Age: ")
+
+# Print the output
+print("Hi " + name + ", you are " + age + " years old.")
