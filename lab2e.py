d26017a92f94cde33dd4ddcf9ce5d8d4f42a7f97
amohamed176 <amohamed176@myseneca.ca>
2025-02-03 17:20:17 -0500

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Umal Khayr Mohamed
+# Author ID: amohamed176
+# Date Created: 2025/02/03
+
+# Set the initial value of the timer
+timer = 10
+
+# Loop until timer reaches 0
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1
+
+# Print "blast off!" after the loop ends
+print("blast off!")
