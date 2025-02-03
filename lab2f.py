3f3e1ff71c9253a4011bc0f10513bf83fc45ab02
amohamed176 <amohamed176@myseneca.ca>
2025-02-03 17:28:34 -0500

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Author: Umal Khayr Mohamed
+# Author ID: amohamed176
+# Date Created: 2025/02/03
+# Set the initial value of the timer
+#timer = 10
+# Loop until timer reaches 0
+#while timer > 0:
+    #print(timer)
+    #timer -= 1  # Decrement the timer by 1
+# Print "blast off!" after the loop ends
+#print("blast off!")
+
+#!/usr/bin/env python3
+
+# Author: Umal Khayr Mohamed
+# Author ID: amohamed176
+# Date Created: 2025/02/03
+
+import sys
+
+# Check if exactly 1 argument is provided
+if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <timer_value>")
+    sys.exit(1)  # Exit the script with an error code
+
+# Assign the first argument to the timer object
+timer = int(sys.argv[1])
+
+# Loop until timer reaches 0
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1
+
+# Print "blast off!" after the loop ends
+print("blast off!")
